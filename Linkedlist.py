fa83e6e4bc986ca2b65065fde8c9950eb500364d
Rellwwww <ldlqxx827@gmail.com>
2026-01-02 15:37:13 +0000

    30/12-1/2

--- Linkedlist.py
+++ Linkedlist.py
@@ -152,6 +152,135 @@ class DoublyLinkedList:
     return old_val
 
 
+#SinglylinkedList
+class SinglyLinkedList:
+  class Node:
+    def __init__(self,val):
+      self.val=val
+      self.next=None
+  
+  def __init__(self):
+    self.head=self.Node(None)
+    self.tail=self.head
+    self.size=0
+#tool APIs
+  def get_size(self):
+    return self.size
+  
+  def is_empty(self):
+    return self.size==0
+  
+  def is_element_index(self,index):
+    return 0<=index<self.size
+  
+  def is_position_index(self,index):
+    return 0<=index<=self.size
+  
+  def check_element_index(self,index):
+    if not self.is_element_index(index):
+      raise IndexError(f"Index:{index},Size:{self.size}")
+    
+  def check_position_index(self,index):
+    if not self.is_position_index(index):
+      raise IndexError(f"Index:{index},Size:{self.size}")
+
+# add element    
+  def add_first(self,elememt):
+    new_node=self.Node(elememt)
+    new_node.next=self.head.next
+    self.head.next=new_node
+    if self.size==0:
+      self.tail=new_node
+    self.size+=1
+
+  def add_last(self,element):
+    new_node=self.Node(element)
+    self.tail.next=new_node
+    self.tail=new_node
+    self.size+=1
+
+  def add_index(self,index,element):
+    self.check_position_index(index)
+    if index==self.size:
+      self.add_last(element)
+      return 
+    p=self.head
+    for _ in range(index):
+      p=p.next
+    new_node=self.Node(element)
+    new_node.next=p.next
+    p.next=new_node
+    self.size+=1
+
+  def remove_first(self):
+    if self.is_empty():
+      raise Exception("No element in list")
+    p=self.head.next
+    self.head.next=p.next
+    if self.size==1:
+      self.tail=self.head
+    self.size-=1
+    return p.val
+  
+  def remove_last(self):
+    if self.is_empty():
+      raise Exception("No element in list") 
+    p=self.head
+    while p.next !=self.tail:
+      p=p.next
+    val=self.tail.val
+    self.tail=p
+    p.next=None
+    self.size-=1
+    return val
+  
+  def remove(self,index):
+    self.check_element_index(index)
+    p=self.head
+    for _ in range(index):
+      p=p.next
+    temp=p.next
+    p.next=temp.next
+    if index==self.size-1
+      self.tail=p
+    self.size-=1
+    return temp.val
+#find element  
+  def get_first(self):
+    if self.is_empty():
+      raise Exception("No element exception")
+    return self.head.next.val
+  
+  def get_last(self):
+    if self.is_empty():
+      raise Exception("No element excetion")
+    return self.tail.val
+  
+  def get(self,index):
+    self.check_element_index(index)
+    p=self.head
+    for _ in range(index):
+      p=p.next
+    return p.val
+  
+#change element  
+  def set(self,index,element):
+    self.check_element_index(index)
+    p=self.head
+    for _ in range(index):
+      p=p.next
+    old_val=p.val
+    p.val=element
+    return old_val
+
+
+
+
+
+  
+
+  
+
 if __name__=="__main__":
   list = DoublyLinkedList()
   list.add_last(1)
